894d6cc6962e71154f61c6224adbc1c47402d802
Andrés Correa Casablanca <andreu@adsmurai.com>
2017-08-07 19:17:37 +0200

    Rename import in Money implementation [skip ci]

--- src/Money.php
+++ src/Money.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Adsmurai\Currency;
 
-use Adsmurai\Currency\Contracts\Money as MoneyInterface;
+use Adsmurai\Currency\Contracts\Money as MoneyContract;
 use Adsmurai\Currency\Contracts\MoneyFormat as MoneyFormatInterface;
-use Adsmurai\Currency\Contracts\Currency;
+use Adsmurai\Currency\Contracts\Currency as CurrencyContract;
 use InvalidArgumentException;
 use Litipk\BigNumbers\Decimal;
 use Litipk\BigNumbers\Errors\InfiniteInputError;
 use Litipk\BigNumbers\Errors\NaNInputError;
 
-final class Money implements MoneyInterface
+final class Money implements MoneyContract
 {
     const DECIMAL_NUMBER_REGEXP = '(?P<amount> 0*(([1-9][0-9]*|[0-9])(\.[0-9]+)?))';
     const SIMPLE_CURRENCY_PATTERN = '/^'.self::DECIMAL_NUMBER_REGEXP.'$/x';
@@ -21,14 +21,14 @@ final class Money implements MoneyInterface
     /** @var Decimal */
     private $amount;
 
-    /** @var Currency */
+    /** @var CurrencyContract */
     private $currencyType;
 
     /**
      * @param Decimal      $amount
-     * @param Currency $currencyType
+     * @param CurrencyContract $currencyType
      */
-    private function __construct(Decimal $amount, Currency $currencyType)
+    private function __construct(Decimal $amount, CurrencyContract $currencyType)
     {
         if ($amount->isNegative()) {
             throw new InvalidArgumentException('Currency amounts must be positive');
@@ -38,7 +38,7 @@ final class Money implements MoneyInterface
         $this->currencyType = $currencyType;
     }
 
-    public static function fromFloat(float $amount, Currency $currencyType): Money
+    public static function fromFloat(float $amount, CurrencyContract $currencyType): Money
     {
         try {
             return new self(
@@ -52,7 +52,7 @@ final class Money implements MoneyInterface
         }
     }
 
-    public static function fromFractionalUnits(int $amount, Currency $currencyType): Money
+    public static function fromFractionalUnits(int $amount, CurrencyContract $currencyType): Money
     {
         $decimalAmount = Decimal::fromInteger($amount)
             ->div(
@@ -63,7 +63,7 @@ final class Money implements MoneyInterface
         return new self($decimalAmount, $currencyType);
     }
 
-    public static function fromString(string $amount, Currency $currencyType): Money
+    public static function fromString(string $amount, CurrencyContract $currencyType): Money
     {
         return new self(
             self::extractNumericAmount($amount, $currencyType),
@@ -71,7 +71,7 @@ final class Money implements MoneyInterface
         );
     }
 
-    private static function extractNumericAmount(string $amount, Currency $currencyType): Decimal
+    private static function extractNumericAmount(string $amount, CurrencyContract $currencyType): Decimal
     {
         try {
             if (
@@ -89,27 +89,27 @@ final class Money implements MoneyInterface
     }
 
     /**
-     * @param Currency $currencyType
+     * @param CurrencyContract $currencyType
      *
      * @return string
      */
-    private static function getAmountPlusIsoCodePattern(Currency $currencyType): string
+    private static function getAmountPlusIsoCodePattern(CurrencyContract $currencyType): string
     {
         $amountPlusIsoCodePattern = '/^'.self::DECIMAL_NUMBER_REGEXP.'\s*'.$currencyType->getISOCode().'$/x';
 
         return $amountPlusIsoCodePattern;
     }
 
-    private static function getAmountPlusSymbolPattern(Currency $currencyType): string
+    private static function getAmountPlusSymbolPattern(CurrencyContract $currencyType): string
     {
         $escapedSymbol = \preg_quote($currencyType->getSymbol());
 
-        return (Currency::BEFORE_PLACEMENT === $currencyType->getSymbolPlacement())
+        return (CurrencyContract::BEFORE_PLACEMENT === $currencyType->getSymbolPlacement())
             ? '/^'.$escapedSymbol.'\s*'.self::DECIMAL_NUMBER_REGEXP.'$/x'
             : '/^'.self::DECIMAL_NUMBER_REGEXP.'\s*'.$escapedSymbol.'$/x';
     }
 
-    public static function fromDecimal(Decimal $amount, Currency $currencyType): Money
+    public static function fromDecimal(Decimal $amount, CurrencyContract $currencyType): Money
     {
         return new self(
             Decimal::fromDecimal($amount, self::INNER_FRACTIONAL_DIGITS),
@@ -117,7 +117,7 @@ final class Money implements MoneyInterface
         );
     }
 
-    public function getCurrencyType(): Currency
+    public function getCurrencyType(): CurrencyContract
     {
         return $this->currencyType;
     }
@@ -190,7 +190,7 @@ final class Money implements MoneyInterface
                 return $number.$separator.$this->currencyType->getISOCode();
                 break;
             default:
-                return (Currency::BEFORE_PLACEMENT === $this->currencyType->getSymbolPlacement())
+                return (CurrencyContract::BEFORE_PLACEMENT === $this->currencyType->getSymbolPlacement())
                     ? $this->currencyType->getSymbol().$separator.$number
                     : $number.$separator.$this->currencyType->getSymbol();
         }
@@ -199,7 +199,7 @@ final class Money implements MoneyInterface
     /**
      * {@inheritdoc}
      */
-    public function equals(MoneyInterface $currency): bool
+    public function equals(MoneyContract $currency): bool
     {
         return $currency === $this || (
                 $this->amount->equals($currency->getAmountAsDecimal()) &&
